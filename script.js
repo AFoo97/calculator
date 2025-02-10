36566fadd104211d2e79e23c1cc5d07902087d1f
AFoo97 <arthur.foozb@gmail.com>
2025-02-10 15:54:16 +1100

    Added get display element and function to update display

--- script.js
+++ script.js
@@ -1,39 +1,11 @@
-function add(a, b) {
-    return a + b;
-}
+let display = document.getElementById("display");
+let currentInput = "";
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return b !== 0 ? a / b : "Error";
-}
-
-let num1 = 8;
-let operator = '+';
-let num2 = 5;
-
-let result;
-switch (operator) {
-    case '+':
-        result = add(num1, num2);
-        break;
-    case '-':
-        result = subtract(num1, num2);
-        break;
-    case '*':
-        result = multiply(num1, num2);
-        break;
-    case '/':
-        result = divide(num1, num2);
-        break;
-    default:
-        result = "Error: Invalid operator.";
+function updateDisplay(value) {
+    display.textContent = value;
 }
 
 function operate(operator, num1, num2) {
