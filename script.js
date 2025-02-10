fc6800442b1b75457ba12eefafd83d33713a89f9
AFoo97 <arthur.foozb@gmail.com>
2025-02-10 15:32:54 +1100

    Created three variables for a number, an operator, and another number

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ function multiply(a, b) {
 function divide(a, b) {
     return b !== 0 ? a / b : "Error";
 }
+
+let num1 = 8;
+let operator = '+';
+let num2 = 5;
+
+let result;
+switch (operator) {
+    case '+':
+        result = add(num1, num2);
+        break;
+    case '-':
+        result = subtract(num1, num2);
+        break;
+    case '*':
+        result = multiply(num1, num2);
+        break;
+    case '/':
+        result = divide(num1, num2);
+        break;
+    default:
+        result = "Error: Invalid operator.";
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+": return num1 + num2;
+        case "-": return num1 - num2;
+        case "*": return num1 * num2;
+        case "/": return num2 !== 0 ? num1 / num2 : "Error"; /* Prevent division by zero */
+        default: return "Error";
+    }
+}
+
+console.log(`${num1} ${operator} ${num2} = ${result}`);
